bb121ec5a3f7f4fdbe0a34f4fb202e77589d6d0c
Harish Karumanchi <harishkarumanchi@Harishs-MacBook-Pro.local>
2016-12-14 23:58:10 -0600

    where method accepts more than one condition so I have utilised where method with two conditions

--- refactor.rb
+++ refactor.rb
@@ -94,7 +94,7 @@ namespace :accounts do
 	
 	desc "Remove accounts where the email was never validated and it is over 30 days old"
 	task :remove_unvalidated do
-		@people = Person.where('created_at < ?', Time.now - 30.days).where(:validated => false)
+		@people = Person.where('created_at < ? AND validated = ?', Time.now - 30.days, false)
 		@people.each do |person|
 			Rails.logger.info "Removing unvalidated user #{person.email}"
 			person.destroy
